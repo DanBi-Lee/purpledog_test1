47156f4b83fd0d60bc5b6bb4045f3d9bf2bd79d6
DanBi-Lee <danbi.s.rain@gmail.com>
2022-04-08 01:25:43 +0900

    [#3] 메인 배너 슬라이드 기능 추가 resolves #3

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import styles from './MainBanner.module.css';
+import {Pagination, A11y} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import "swiper/css/pagination";
 
 function MainBanner() {
     return (
         <section className={styles.main_banner}>
-            <div className={styles.slide}></div>
+            <Swiper
+                modules={[Pagination, A11y]}
+                pagination={{
+                type: "progressbar",
+            }}
+                className={styles.swiper}>
+                <SwiperSlide>
+                    <article className={styles.swiper__item}>
+                        <div className={styles.content__wrap}>
+                            <h3>01</h3>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className={styles.swiper__item}>
+                        <div className={styles.content__wrap}>
+                            <h3>02</h3>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className={styles.swiper__item}>
+                        <div className={styles.content__wrap}>
+                            <h3>03</h3>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className={styles.swiper__item}>
+                        <div className={styles.content__wrap}>
+                            <h3>04</h3>
+                        </div>
+                    </article>
+                </SwiperSlide>
+            </Swiper>
         </section>
     );
 }
